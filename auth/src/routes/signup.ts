170b1c684834db0c6d864463d65c234e3fc26cdd
Aidan Dang <aidandang@gmail.com>
2022-10-29 07:02:03 -0700

    completed Section 7

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -11,7 +11,7 @@ router.post(
 		body('email').isEmail().withMessage('Email must be valid'),
 		body('password').trim().isLength({ min: 4, max: 20 }),
 	],
-	(req: Request, res: Response) => {
+	async (req: Request, res: Response) => {
 		const errors = validationResult(req);
 
 		if (!errors.isEmpty()) {
