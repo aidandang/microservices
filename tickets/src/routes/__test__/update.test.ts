00ea5e88f25b7d26c69b3690dffd9e0143c20a45
Aidan Dang <aidandang@gmail.com>
2022-11-20 06:45:52 -0800

    finished section-16

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('returns a 404 if the provided id does not exit', async () => {
 	const id = new mongoose.Types.ObjectId().toHexString();
@@ -93,4 +94,29 @@ it('updates the ticket provided valid inputs', async () => {
 
 	expect(ticketResponse.body.title).toEqual(title);
 	expect(ticketResponse.body.price).toEqual(price);
+});
+
+it('publishes an event', async () => {
+	const cookie = signup();
+	const title = 'sdfdsfdsfds';
+	const price = 100;
+
+	const response = await request(app)
+		.post(`/api/tickets`)
+		.set('Cookie', cookie)
+		.send({
+			title: 'dsfdsfsd',
+			price: 20,
+		});
+
+	await request(app)
+		.put(`/api/tickets/${response.body.id}`)
+		.set('Cookie', cookie)
+		.send({
+			title,
+			price,
+		})
+		.expect(200);
+
+	expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
