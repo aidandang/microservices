91e8825dcdb1be4a53d1e2a9e35fbc65c57e9f5e
Aidan Dang <aidandang@gmail.com>
2022-12-25 23:45:52 -0800

    done

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -8,7 +8,6 @@ import { app } from './app';
 mongoose.set('strictQuery', true);
 
 const start = async () => {
-	console.log('hoeelo');
 	if (!process.env.JWT_KEY) {
 		throw new Error('JWT_KEY must be defined');
 	}
